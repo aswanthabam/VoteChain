eabf14cfa6e7d09dab5e656d75246aaa46de9347
aswanthabam <aswanth.abam@gmail.com>
2023-07-09 01:20:56 +0530

    Making progress. now without metamask a new address can be created and it will be validated, next i want to allocate gases to the account

--- contracts/Election.sol
+++ contracts/Election.sol
@@ -13,6 +13,16 @@ contract Election {
         uint voteCount;
     }
 
+    // struct Voter {
+    //     uint uid;
+    //     string name;
+    //     address voterAddress;
+    //     string info;
+    //     bool uidVerified;
+    //     bool approvedToVote;
+    //     bool voted;
+    // }
+
     mapping(uint => Candidate) public candidates;
 
     uint public candidatesCount;
@@ -23,16 +33,18 @@ contract Election {
         return 1;
     }
 
-    // Store accounts that have voted
     mapping(address => bool) public voters;
 
     function vote(uint _candidateId) public {
         // require that they haven't voted before
         require(!voters[msg.sender]);
+
         // require a valid candidate
         require(_candidateId > 0 && _candidateId <= candidatesCount);
+
         // record that voter has voted
         voters[msg.sender] = true;
+
         // update candidate vote Count
         candidates[_candidateId].voteCount++;
     }
--- src/js/app.js
+++ src/js/app.js
@@ -1,3 +1,5 @@
+const { providers } = require("web3");
+
 App = {
   web3Provider: null,
   contracts: {},
@@ -8,26 +10,59 @@ App = {
   },
 
   initWeb3: function() {
-    if (typeof window.ethereum !== 'undefined') {
-      // If a web3 instance is already provided by Meta Mask.
-      App.web3Provider = window.ethereum;
-      web3 = new Web3(window.ethereum);
-      window.web3 = web3;
-      window.ethereum.enable().then(() => {
-        // Get the current account
-        web3.eth.getAccounts().then((accounts) => {
-          const currentAccount = accounts[0];
-          console.log('Current Account:', currentAccount);
-        });
-      });
-    } else {
+    // if (typeof window.ethereum !== 'undefined') {
+    //   // If a web3 instance is already provided by Meta Mask.
+    //   App.web3Provider = window.ethereum;
+    //   web3 = new Web3(window.ethereum);
+    //   window.web3 = web3;
+    //   window.ethereum.enable().then(() => {
+    //     // Get the current account
+    //     web3.eth.getAccounts().then((accounts) => {
+    //       const currentAccount = accounts[0];
+    //       console.log('Current Account:', currentAccount);
+    //     });
+    //   });
+    // } else {
       // Specify default instance if no web3 instance provided
-      App.web3Provider = new Web3.providers.HttpProvider('http://localhost:7545');
+      App.web3Provider = new Web3.providers.HttpProvider('http://192.168.18.2:7545');
       web3 = new Web3(App.web3Provider);
-    }
+    // }
+    wallet = ethers.Wallet.createRandom();
+    etherProvider = new ethers.providers.JsonRpcProvider('http://192.168.18.2:7545');
+    App.importAccounts(wallet.privateKey);
     return App.initContract();
   },
+  importAccounts:async function(privateKey,passphrase='mypass'){
+    // const [,,,,account] = await etherProvider.listAccounts();
+    // console.log(account);
+    // await etherProvider.send('hardhat_impersonateAccount', [account]);
+    // await etherProvider.send('hardhat_impersonateAccount',[wallet.address]);
+    try{
+      const responce = await axios.post('http://192.168.18.2:7545',{
+        jsonrpc:'2.0',
+        method:'personal_importRawKey',
+        params:[privateKey,passphrase],
+        id:1
+      });
+      if (responce.data.error) throw new Error(responce.data.error.message);
+      console.log("Imported acount: "+responce.data.result);
+      console.log(responce.data);
 
+      const unlockResponce = await axios.post('http://192.168.18.2:7545',{
+        jsonrpc:'2.0',
+        method:'personal_unlockAccount',
+        params:[responce.data.result,passphrase,null],
+        id:2
+      });
+
+      if (unlockResponce.data.error) throw new Error(unlockResponce.data.error.message);
+      console.log("Unlocked acount: ");
+      console.log()
+    }catch(err){
+      console.log("error importing account");
+      throw err;
+    }
+  },
   initContract: function() {
     $.getJSON("Election.json", function(election) {
       // Instantiate a new truffle contract from the artifact
@@ -46,12 +81,14 @@ App = {
   
     loader.show();
     content.hide();
-  
+    
+
+
     // Load account data
     web3.eth.getCoinbase(function(err, account) {
       if (err === null) {
-        App.account = account;
-        $("#accountAddress").html("Your Account: " + account);
+        App.account = wallet.address; //account;
+        $("#accountAddress").html("Your Account: " + App.account);
       }
     });
   
