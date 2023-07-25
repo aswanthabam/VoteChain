c4d998a38d9c1279692cc0a7a728ade5ee34286a
aswanthabam <aswanth.abam@gmail.com>
2023-07-25 13:39:01 +0530

    new contract updated

--- blockchain/contracts/Election.sol
+++ blockchain/contracts/Election.sol
@@ -12,17 +12,34 @@ contract Election {
         string name;
         uint voteCount;
     }
-
+    
+    struct ElectionEntity {
+      uint id;
+      string name;
+    }
+    event UserRegisteredEvent(address _address);
+    struct User {
+      uint uid;
+      address _address;
+      string name;
+      bool approved;
+    }
+    mapping(address => User) public users;
+    mapping(uint => bool) private isRegistered;
+    mapping(address => bool) public approvedToVote;
+    
+    mapping(address => ElectionEntity) public allowedElections;
+    mapping(uid => Candidate[]) public candidatesOf;
+    
+    function registerUser(uint uid, string memory _name) {
+      require(users[msg.sender] == null); // The address is not used
+      require(!isRegistered[uid]); // The uid is not registered
+      users[msg.sender] = User(uid,msg.sender,_name,false);
+      isRegistered[uid] = true;
+      emit UserRegisteredEvent(msg.sender);
+    }
+    
     event VotedEvent(address _from);
-    // struct Voter {
-    //     uint uid;
-    //     string name;
-    //     address voterAddress;
-    //     string info;
-    //     bool uidVerified;
-    //     bool approvedToVote;
-    //     bool voted;
-    // }
 
     mapping(uint => Candidate) public candidates;
 
