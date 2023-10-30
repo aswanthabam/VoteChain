bdc4979c22a2bc766767a7cb86fd98ec91ebe2d6
aswanthabam <aswanth.abam@gmail.com>
2023-10-30 14:52:29 +0530

    added(async_button) : reusable async button

---
+++ lib/components/buttons/async_button.dart
@@ -0,0 +1,263 @@
+import 'package:flutter/material.dart';
+
+class AsyncButtonState {
+  static const int INITED = 0;
+  static const int LOADING = 1;
+  static const int LOADED = 2;
+  static const int FAILED = 3;
+  Function stateFunction;
+  AsyncButtonState(
+      {required state,
+      required this.primaryBackground,
+      required this.primaryTextColor,
+      required this.loadingBackground,
+      required this.loadingTextColor,
+      required this.failedTextColor,
+      required this.failedBackground,
+      required this.successBackground,
+      required this.successTextColor,
+      required this.progressBarColor,
+      required this.primaryText,
+      required this.loadingText,
+      required this.failedText,
+      required this.successText,
+      required this.stateFunction}) {
+    _state = state;
+  }
+  late int _state;
+
+  final Color primaryBackground;
+  final Color primaryTextColor;
+  final Color loadingBackground;
+  final Color loadingTextColor;
+  final Color failedBackground;
+  final Color failedTextColor;
+  final Color successBackground;
+  final Color successTextColor;
+  final Color progressBarColor;
+  final String primaryText;
+  final String loadingText;
+  final String failedText;
+  final String successText;
+
+  void setState(int state) {
+    stateFunction(() {
+      _state = state;
+    });
+  }
+
+  Color getBackgroundColor() {
+    switch (_state) {
+      case AsyncButtonState.INITED:
+        return primaryBackground;
+      case AsyncButtonState.LOADING:
+        return loadingBackground;
+      case AsyncButtonState.LOADED:
+        return successBackground;
+      case AsyncButtonState.FAILED:
+        return failedBackground;
+      default:
+        return primaryBackground;
+    }
+  }
+
+  Color getColor() {
+    switch (_state) {
+      case AsyncButtonState.INITED:
+        return primaryTextColor;
+      case AsyncButtonState.LOADING:
+        return loadingTextColor;
+      case AsyncButtonState.LOADED:
+        return successTextColor;
+      case AsyncButtonState.FAILED:
+        return failedTextColor;
+      default:
+        return primaryTextColor;
+    }
+  }
+
+  String getText() {
+    switch (_state) {
+      case AsyncButtonState.INITED:
+        return primaryText;
+      case AsyncButtonState.LOADING:
+        return loadingText;
+      case AsyncButtonState.LOADED:
+        return successText;
+      case AsyncButtonState.FAILED:
+        return failedText;
+      default:
+        return primaryText;
+    }
+  }
+
+  bool isLoading() {
+    return _state == LOADING ? true : false;
+  }
+}
+
+class AsyncButton extends StatefulWidget {
+  const AsyncButton(
+      {super.key,
+      required this.onPressed,
+      required this.primaryBackground,
+      required this.primaryTextColor,
+      required this.loadingBackground,
+      required this.loadingTextColor,
+      required this.failedTextColor,
+      required this.failedBackground,
+      required this.successBackground,
+      required this.successTextColor,
+      required this.progressBarColor,
+      required this.primaryText,
+      required this.loadingText,
+      required this.failedText,
+      required this.successText,
+      required this.width});
+
+  final Future<bool> Function() onPressed;
+
+  final Color primaryBackground;
+  final Color primaryTextColor;
+  final Color loadingBackground;
+  final Color loadingTextColor;
+  final Color failedBackground;
+  final Color failedTextColor;
+  final Color successBackground;
+  final Color successTextColor;
+  final Color progressBarColor;
+  final String primaryText;
+  final String loadingText;
+  final String failedText;
+  final String successText;
+  final double width;
+  @override
+  State<AsyncButton> createState() => _AsyncButtonState();
+}
+
+class _AsyncButtonState extends State<AsyncButton> {
+  late AsyncButtonState state;
+  @override
+  void initState() {
+    super.initState();
+    state = AsyncButtonState(
+        stateFunction: setState,
+        state: AsyncButtonState.INITED,
+        primaryBackground: widget.primaryBackground,
+        primaryTextColor: widget.primaryTextColor,
+        loadingBackground: widget.loadingBackground,
+        loadingTextColor: widget.loadingTextColor,
+        failedTextColor: widget.failedTextColor,
+        failedBackground: widget.failedBackground,
+        successBackground: widget.successBackground,
+        successTextColor: widget.successTextColor,
+        progressBarColor: widget.progressBarColor,
+        primaryText: widget.primaryText,
+        loadingText: widget.loadingText,
+        failedText: widget.failedText,
+        successText: widget.successText);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return TextButton(
+      onPressed: () {
+        state.setState(AsyncButtonState.LOADING);
+        widget.onPressed().then((value) {
+          if (value) {
+            state.setState(AsyncButtonState.LOADED);
+          } else {
+            state.setState(AsyncButtonState.FAILED);
+          }
+        });
+      },
+      child: Container(
+        height: 50,
+        width: widget.width,
+        padding: const EdgeInsets.all(15),
+        decoration: BoxDecoration(
+            color: state.getBackgroundColor(),
+            borderRadius: BorderRadius.circular(30)),
+        child: Stack(
+          children: [
+            Row(
+              children: [
+                Expanded(
+                    child: Center(
+                  child: Row(
+                    mainAxisSize: MainAxisSize.min,
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: [
+                      const SizedBox(width: 16),
+                      Text(
+                        state.getText(),
+                        style: TextStyle(
+                          color: state.getColor(),
+                          fontSize: 16,
+                          fontFamily: 'Poppins',
+                          fontWeight: FontWeight.w600,
+                          letterSpacing: 0.16,
+                        ),
+                      ),
+                      const SizedBox(width: 10),
+                      SizedBox(
+                        width: 12,
+                        height: 12,
+                        child: state.isLoading()
+                            ? const CircularProgressIndicator(
+                                color: Colors.white,
+                                strokeWidth: 3,
+                              )
+                            : const SizedBox(),
+                      )
+                    ],
+                  ),
+                )),
+              ],
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+AsyncButton getMinimalAsyncButton(
+    context,
+    Future<bool> Function() onPressed,
+    String primaryText,
+    String failedText,
+    String loadingText,
+    String successText,
+    Color background,
+    Color textColor,
+    double width) {
+  return AsyncButton(
+      width: width,
+      onPressed: onPressed,
+      primaryBackground: background,
+      primaryTextColor: textColor,
+      loadingBackground: background,
+      loadingTextColor: textColor,
+      failedTextColor: textColor,
+      failedBackground: background,
+      successBackground: background,
+      successTextColor: textColor,
+      progressBarColor: textColor,
+      primaryText: primaryText,
+      loadingText: loadingText,
+      failedText: failedText,
+      successText: successText);
+}
+
+AsyncButton getPrimaryAsyncButton(
+    context,
+    Future<bool> Function() onPressed,
+    String primaryText,
+    String loadingText,
+    String failedText,
+    String successText,
+    double width) {
+  return getMinimalAsyncButton(context, onPressed, primaryText, failedText,
+      loadingText, successText, Colors.blue, Colors.white, width);
+}
--- lib/pages/register.dart
+++ lib/pages/register.dart
@@ -1,13 +1,7 @@
 // ignore_for_file: use_build_context_synchronously
 
 import 'package:flutter/material.dart';
-import '../components/keyboard.dart';
-import '../components/dialog.dart';
-import '../components/uid.dart';
-import '../components/otp.dart';
-import '../components/password.dart';
-import '../classes/global.dart';
-import '../classes/api.dart';
+import '../components/buttons/async_button.dart';
 
 class Register extends StatefulWidget {
   const Register({super.key});
@@ -22,148 +16,19 @@ class _RegisterState extends State<Register> {
   String otp = "";
   String password = "";
 
-  bool displayIntKeyboard = false, keyboardReset = false;
-  bool buttonEnabled = true, buttonProcessing = false;
-  int keyboardLength = 12;
-  final TextEditingController _keyboardController = TextEditingController();
-  final FocusNode _keyboard = FocusNode();
-
   @override
   void initState() {
     super.initState();
-  }
-
-  /* Show the numeric keyboard */
-  void showNumKeyboard(int size) {
-    setState(() {
-      displayIntKeyboard = true;
-      keyboardLength = size;
-    });
-  }
-
-  /* Hide the numeric keyboard */
-  void hideNumKeyboard() {
-    setState(() {
-      displayIntKeyboard = false;
-    });
-  }
-
-  /* Show the alpha numeric keyboard */
-  void showAlphaKeyboard(int size) {
-    setState(() {
-      displayIntKeyboard = false;
-      keyboardLength = size;
-    });
-    FocusScope.of(context).requestFocus(_keyboard);
-  }
-
-  /* Hide alpha numeric keyboard */
-  void hideAlphaKeyboard() {
-    _keyboard.unfocus();
-  }
-
-  /* Get the current value entered by the user (Eg : otp if user is entering otp) */
-  String getCurrentValue() {
-    switch (step) {
-      case 0:
-        return uid;
-      case 1:
-        return otp;
-      case 2:
-        return password;
-      default:
-        return "";
-    }
-  }
-
-  /* Add value to the current value 
-    Character by character adding */
-  void addValue(String val, int step) {
-    String tmp = getCurrentValue();
-    if (val == "clr") {
-      tmp = "";
-    } else if (val == "bck") {
-      if (tmp.isNotEmpty) {
-        tmp = tmp.substring(0, tmp.length - 1);
-      }
-    } else {
-      tmp += val;
-    }
-    setValue(tmp);
-  }
-
-  /* Set the value of current entering value directly */
-  void setValue(String val) {
-    setState(() {
-      switch (step) {
-        case 0:
-          uid = val;
-          break;
-        case 1:
-          if (val.length <= 6) {
-            otp = val;
-          }
-          break;
-        case 2:
-          if (val.isNotEmpty) {
-            password = val;
-          }
-          break;
-      }
-    });
   }
 
   /* Get the current widget
     Eg : OTP widget if in the otp entering step */
   Widget getCurrentWidget() {
     switch (step) {
-      case 0:
-        showNumKeyboard(12);
-        return UID(
-          value: uid,
-        );
-      case 1:
-        hideNumKeyboard();
-        return OTP(
-          value: otp,
-          onInputClick: () {
-            showAlphaKeyboard(6);
-          },
-        );
-      case 2:
-        hideNumKeyboard();
-        hideAlphaKeyboard();
-        return Password(
-          onChange: (val) {
-            setValue(val);
-          },
-        );
       default:
         return const Expanded(
           child: SizedBox(),
         );
-    }
-  }
-
-  /* Clear the value of the current step */
-  void clearStep() {
-    switch (step) {
-      case 0:
-        setState(() {
-          uid = "";
-          keyboardLength = 0;
-        });
-      case 1:
-        setState(() {
-          otp = "";
-          keyboardLength = 12;
-          _keyboardController.text = "";
-        });
-      case 2:
-        setState(() {
-          password = "";
-          keyboardLength = 6;
-        });
     }
   }
 
@@ -189,36 +54,6 @@ class _RegisterState extends State<Register> {
     }
   }
 
-  /* Validate the entered value 
-    Validate using the current step on which the user is on */
-  bool validate() {
-    switch (step) {
-      case 0:
-        if (uid.length == 12) {
-          return true;
-        } else {
-          return false;
-        }
-      case 1:
-        if (otp.length == 6) {
-          return true;
-        } else {
-          return false;
-        }
-      case 2:
-        if (password.length > 7 &&
-            RegExp(r'[a-z]').hasMatch(password) &&
-            RegExp(r'[A-Z]').hasMatch(password) &&
-            RegExp(r'[0-9]').hasMatch(password)) {
-          return true;
-        } else {
-          return false;
-        }
-      default:
-        return false;
-    }
-  }
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -235,7 +70,7 @@ class _RegisterState extends State<Register> {
           top: 30,
           child: IconButton(
               onPressed: () {
-                clearStep();
+                // clearStep();
                 if (!preStep()) Navigator.pop(context);
               },
               icon: const Icon(Icons.arrow_back_ios))),
@@ -246,174 +81,17 @@ class _RegisterState extends State<Register> {
               onPressed: () {}, icon: const Icon(Icons.help_outline_outlined))),
       Positioned(
           top: 100,
+          height: MediaQuery.of(context).size.height - 100,
           child: Column(
             children: [
               getCurrentWidget(),
-              TextButton(
-                onPressed: () async {
-                  if (validate()) {
-                    if (step == totalstep - 1) {
-                      hideAlphaKeyboard();
-                      setState(() {
-                        // buttonEnabled = false;
-                        buttonProcessing = true;
-                      });
-                      await Global.linker.requestEthers(
-                          onSuccess: (val) {
-                            showDialog(
-                                context: context,
-                                builder: (BuildContext context) => Dialog(
-                                    shape: RoundedRectangleBorder(
-                                        borderRadius:
-                                            BorderRadius.circular(20)),
-                                    child: Padding(
-                                        padding: const EdgeInsets.all(10),
-                                        child: Column(
-                                            mainAxisSize: MainAxisSize.min,
-                                            children: [
-                                              Center(child: Text(val))
-                                            ]))));
-                          },
-                          onError: (val) {});
-                      var st = await Global.linker
-                          .register("TEst", int.parse(uid), onError: (val) {
-                        showDialog(
-                            context: context,
-                            builder: ((context) => MsgDialog(
-                                icon: Icons.error_outline,
-                                iconColor: Colors.red,
-                                iconSize: 30,
-                                text:
-                                    "An Unexpected error occured while creating account")));
-                      });
-                      if (st) {
-                        var value = await API.registerUser(
-                            (await Global.linker.getAddress()).toString(),
-                            int.parse(uid),
-                            password);
-                        if (value) {
-                          Global.linker.saveWallet(
-                              "Aswanth V C", int.parse(uid), password);
-                          showDialog(
-                              context: context,
-                              builder: ((context) => MsgDialog(
-                                  icon: Icons.done_rounded,
-                                  iconColor: Colors.green.shade400,
-                                  iconSize: 30,
-                                  text: "Created Account Successfully")));
-                          Navigator.pushReplacementNamed(context, 'home');
-                        } else {
-                          showDialog(
-                              context: context,
-                              builder: ((context) => MsgDialog(
-                                  icon: Icons.error_outline_rounded,
-                                  iconColor: Colors.red,
-                                  iconSize: 30,
-                                  text:
-                                      "An Unexpected error occured while creating account")));
-                        }
-                      } else {
-                        showDialog(
-                            context: context,
-                            builder: ((context) => MsgDialog(
-                                icon: Icons.error_outline_rounded,
-                                iconColor: Colors.red,
-                                iconSize: 30,
-                                text:
-                                    "An Unexpected error occured while creating account, please try again")));
-                      }
-                      setState(() {
-                        // buttonEnabled = false;
-                        buttonProcessing = false;
-                      });
-                    } else {
-                      nextStep();
-                    }
-                  }
-                },
-                child: Container(
-                  height: 50,
-                  width: MediaQuery.of(context).size.width - 30,
-                  padding: const EdgeInsets.all(15),
-                  decoration: BoxDecoration(
-                      color: validate() && buttonEnabled
-                          ? const Color(0xff1CA78E)
-                          : const Color.fromARGB(170, 208, 255, 247),
-                      borderRadius: BorderRadius.circular(30)),
-                  child: Stack(
-                    children: [
-                      Row(
-                        children: [
-                          Expanded(
-                              child: Center(
-                            child: Row(
-                              mainAxisSize: MainAxisSize.min,
-                              crossAxisAlignment: CrossAxisAlignment.center,
-                              children: [
-                                const SizedBox(width: 16),
-                                Text(
-                                  step == totalstep - 1 ? "Submit" : "Next",
-                                  style: TextStyle(
-                                    color: validate() && buttonEnabled
-                                        ? const Color(0xffffffff)
-                                        : const Color(0x7C1E1E1E),
-                                    fontSize: 16,
-                                    fontFamily: 'Poppins',
-                                    fontWeight: FontWeight.w600,
-                                    letterSpacing: 0.16,
-                                  ),
-                                ),
-                                const SizedBox(width: 10),
-                                SizedBox(
-                                  width: 12,
-                                  height: 12,
-                                  child: buttonProcessing
-                                      ? const CircularProgressIndicator(
-                                          color: Colors.white,
-                                          strokeWidth: 3,
-                                        )
-                                      : const SizedBox(),
-                                )
-                              ],
-                            ),
-                          )),
-                        ],
-                      )
-                    ],
-                  ),
-                ),
-              )
+              getPrimaryAsyncButton(context, () async {
+                await Future.delayed(Duration(seconds: 2));
+                return true;
+              }, "Continue", "Loading", "Failed", "Success",
+                  MediaQuery.of(context).size.width - 20)
             ],
           )),
-      Positioned(
-          bottom: 0,
-          child: displayIntKeyboard
-              ? KeyBoard(
-                  onPressed: (val) {
-                    addValue(val, step);
-                  },
-                  length: keyboardLength,
-                  reset: keyboardReset,
-                  curLength: getCurrentValue().length)
-              : Opacity(
-                  opacity: 0,
-                  child: SizedBox(
-                    width: MediaQuery.of(context).size.width,
-                    // height: 10,
-                    child: TextFormField(
-                      focusNode: _keyboard,
-                      controller: _keyboardController,
-                      maxLength: keyboardLength,
-                      cursorHeight: 0,
-                      // enabled: false,
-                      onChanged: (val) {
-                        if (val.length <= keyboardLength) setValue(val);
-                      },
-                      style: const TextStyle(color: Colors.transparent),
-                      // autofocus: true,
-                    ),
-                  ),
-                )),
     ]));
   }
 }
