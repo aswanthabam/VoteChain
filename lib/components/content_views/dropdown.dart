67f8a3b0f81af9033a2ea6dbfeacb458eff0e4d6
aswanthabam <aswanth.abam@gmail.com>
2023-11-04 19:10:20 +0530

    add : drop down content view

--- lib/components/content_views/dropdown.dart
+++ lib/components/content_views/dropdown.dart
@@ -1,8 +1,24 @@
 import 'package:flutter/material.dart';
 
-class ContentDropdown extends StatefulWidget {
-  const ContentDropdown({super.key});
+abstract class ContentType {
+  Widget build();
+}
 
+class TextContent extends ContentType {
+  TextContent({required this.content});
+
+  String content;
+
+  @override
+  Widget build() {
+    return Text(content);
+  }
+}
+
+class ContentDropdown extends StatefulWidget {
+  ContentDropdown({super.key, required this.heading, required this.contents});
+  String heading;
+  List<ContentType> contents;
   @override
   State<ContentDropdown> createState() => _ContentDropdownState();
 }
@@ -10,6 +26,39 @@ class ContentDropdown extends StatefulWidget {
 class _ContentDropdownState extends State<ContentDropdown> {
   @override
   Widget build(BuildContext context) {
-    return Placeholder();
+    return Row(
+      mainAxisSize: MainAxisSize.max,
+      children: [
+        Expanded(
+            child: Container(
+                padding: const EdgeInsets.all(15),
+                decoration: BoxDecoration(
+                    boxShadow: [
+                      BoxShadow(
+                          blurRadius: 10,
+                          spreadRadius: 10,
+                          color: Colors.grey.shade200)
+                    ],
+                    color: Color.fromARGB(255, 235, 248, 235),
+                    borderRadius: BorderRadius.circular(20)),
+                child: GestureDetector(
+                    onTap: () {
+                      print("Tapped");
+                    },
+                    child: Row(children: [
+                      Text(
+                        widget.heading,
+                        style: TextStyle(
+                            fontSize: 15, fontWeight: FontWeight.w600),
+                      ),
+                      const Spacer(),
+                      Icon(
+                        Icons.keyboard_arrow_down_sharp,
+                        size: 35,
+                        color: Colors.grey,
+                      )
+                    ]))))
+      ],
+    );
   }
 }
